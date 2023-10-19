ec5a75c2a525cfb7143d63420f6e9d4c348d9e01
Begüm Kaya <105585556+soleilsol@users.noreply.github.com>
2023-10-19 14:04:32 +0300

    Add project files.

---
+++ MvcStok/Controllers/SatısController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class SatısController : Controller
+    {
+        MvcDbStockEntities db = new MvcDbStockEntities();
+        // GET: Satıs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
